7aa13d11b320a821bdf5004cb027477d9cfb3703
Sarvesh Kumar <sarvesh.b.kumar@accenture.com>
2022-03-11 22:53:41 +0530

    add yml

--- .sfdx/typings/lwc/apex/RelatedList.d.ts
+++
@@ -1,6 +0,0 @@
-declare module "@salesforce/apex/RelatedList.getRecords" {
-  export default function getRecords(param: {soql: any, SObjectName: any, iconName: any}): Promise<any>;
-}
-declare module "@salesforce/apex/RelatedList.countRecords" {
-  export default function countRecords(param: {objectName: any}): Promise<any>;
-}
