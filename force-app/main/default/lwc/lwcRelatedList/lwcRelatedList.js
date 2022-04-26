5821b72b963f8f83a5b33af6cd709e6c4d7de00c
Sarvesh Kumar <sarvesh.b.kumar@accenture.com>
2022-04-26 21:13:25 +0530

    fix pagination

--- force-app/main/default/lwc/lwcRelatedList/lwcRelatedList.js
+++ force-app/main/default/lwc/lwcRelatedList/lwcRelatedList.js
@@ -194,7 +194,8 @@ export default class LightningDatatable extends NavigationMixin(
   }
 
   get isDisableNext() {
-    return this.offSet + this.limit >= this.totalRows || this.totalRows === 0
+    console.log(this.offSet, this.limit, this.totalRows)
+    return this.offSet + (this.hasPagination ? this.limit > 10 ? this.limit : 10 : this.limit) >= this.totalRows || this.totalRows === 0
       ? true
       : this.totalRows <= this.limit
         ? false
@@ -270,8 +271,7 @@ export default class LightningDatatable extends NavigationMixin(
     let soql = this.appendField();
     soql += this.appendWhere();
     soql += " WITH SECURITY_ENFORCED ";
-    
-    if(this.limit && this.limit > 0) {
+    if (this.limit && this.limit > 0) {
       soql += this.appendLimit();
       soql += this.appendOffset();
     }
