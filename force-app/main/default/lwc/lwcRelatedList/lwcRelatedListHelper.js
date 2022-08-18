377274b89fd8583776e406cd53f6e1100b55bfbf
Sarvesh <sarveshkumar574@gmail.com>
2022-08-18 20:05:42 +0530

    Adding local config data

--- force-app/main/default/lwc/lwcRelatedList/lwcRelatedListHelper.js
+++ force-app/main/default/lwc/lwcRelatedList/lwcRelatedListHelper.js
@@ -1,10 +1,19 @@
-const configLocal = (cmp) => {
-	cmp.title = "LWC Datatable";
-	// cmp.iconName = "standard:account";
-	cmp.fields = "FirstName,LastName,AccountId,Opportunity__c";
-	cmp.objectName = "Contact";
-	cmp.limit = 5;
-	cmp.hasPagination = true;
+const configLocal = (cmp, local) => {
+	if (local) {
+		cmp.title = "Contacts";
+		// cmp.iconName = "standard:account";
+		cmp.fields = "FirstName,LastName,AccountId";
+		cmp.objectName = "Contact";
+		cmp.limit = 5;
+		// cmp.recordId = "recid";
+		// cmp.relatedFieldAPI = "AccountId";
+		// cmp.whereClause;
+		cmp.isCounterDisplayed = true;
+		cmp.actionButtons = '[{"name": "New","label": "New","variant": "neutral"}]'; //buttons for the list
+		cmp.showCheckboxes = true;
+		cmp.showViewAll = false;
+		cmp.hasPagination = false;
+	}
 };
 
 export { configLocal };
