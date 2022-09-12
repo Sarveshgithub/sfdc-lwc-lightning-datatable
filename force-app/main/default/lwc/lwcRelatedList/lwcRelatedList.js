91fdbbd90fcc98c5a1a412dff790a32effe3f516
Sarvesh <sarveshkumar574@gmail.com>
2022-09-12 16:20:23 +0530

    redirect to fetchrecord #91

--- force-app/main/default/lwc/lwcRelatedList/lwcRelatedList.js
+++ force-app/main/default/lwc/lwcRelatedList/lwcRelatedList.js
@@ -119,7 +119,7 @@ export default class LightningDatatable extends NavigationMixin(LightningElement
 			.then(() => {
 				this.showToast("Success", "Record Updated", "success");
 				this.draftValues = [];
-				if (this.recordId) this.showRowDetails({ Id: this.recordId });
+				this.fetchRecords();
 			})
 			.catch((error) => {
 				if (error) {
